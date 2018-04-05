8ef10415617a6af70016b498e9c54815a59d319d
rmhenry <reeselightning83@gmail.com>
2018-04-05 16:25:15 -0500

    Added MainMenu() method to Program.cs

--- HW8/Program.cs
+++ HW8/Program.cs
@@ -10,6 +10,63 @@ namespace HW8
     {
         static void Main(string[] args)
         {
+            MainMenu();
+        }
+
+        public static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Homework 8 \n");
+            Console.WriteLine("Main Menu \n");
+            Console.WriteLine("1 - Debugging Exercise");
+            Console.WriteLine("2 - Exercise 10_9");
+            Console.WriteLine("3 - Exercise 10_13");
+            Console.WriteLine("4 - Exercise 10_17");
+            Console.WriteLine("5 - Exit\n");
+            Console.Write("Please enter a selection: ");
+
+            string userSelection = Console.ReadLine();
+
+            switch (userSelection)
+            {
+                case "1":
+                    DebugTest();
+                    break;
+                case "2":
+                    Ex10_9Test();
+                    break;
+                case "3":
+                    Ex10_13Test();
+                    break;
+                case "4":
+                    Ex10_17Test();
+                    break;
+                default:
+                    Console.Write("Exiting program.");
+                    System.Threading.Thread.Sleep(3000);    //keep console open 3 seconds for user to read exit message
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        public static void DebugTest()
+        {
+
+        }
+
+        public static void Ex10_9Test()
+        {
+
+        }
+
+        public static void Ex10_13Test()
+        {
+
+        }
+
+        public static void Ex10_17Test()
+        {
+
         }
     }
 }
